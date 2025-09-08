562b3c9647b3316870758891c5b4f59bc97bcb18
Jundyk12 <jundykato@gmail.com>
2025-09-08 15:43:18 -0700

    Finalize code

--- Labs/lab4.cpp
+++ Labs/lab4.cpp
@@ -1,131 +1,151 @@
-/*
-*this C++ program prompts the user for their gross income & filing status to then calculate their income tax, standard deduction, and 
-*taxable income and prints the output
-*CECS 275 - Fall 2025
-*author Ivan Tran
-*author Jundy Kato
-*version 6.3.0
-*last updated 9/08/2025
-*/
-
 #include <iostream>
 #include <iomanip>
-
+#include <string>   //Use string instead of char 
 using namespace std;
 
 int main() {
-    double long  gross_income;
+    //Setting the decimals two places
+    cout << fixed << setprecision(2);
+
+    //Declaring data types for each variables
+    double gross_income, calc_income;
+    string tax_rate;                    //Using string instead of char to able to more characters than a single one
     char filing_status;
-    double long calc_income;
-    char tax_rate;
+    double standard_deduction = 0.0;    //Setting standard deduction to 0
+
     cout << "Enter your gross income: ";
     cin >> gross_income;
     cout << "Please enter your filing status (S - single, H - head of household, J - Married filing jointly, P - married filing separately): ";
     cin >> filing_status;
-    calc_income = gross_income;
-    if (filing_status == 'S' || filing_status == 's') { //calc tax for single
+
+    //Standard Deduction and setting each char with their own standard deduction
+    if (filing_status == 'S' || filing_status == 's' || filing_status == 'P' || filing_status == 'p') {
+        standard_deduction = 14600;
+    } else if (filing_status == 'H' || filing_status == 'h') {
+        standard_deduction = 21900;
+    } else if (filing_status == 'J' || filing_status == 'j') {
+        standard_deduction = 29200;
+    }
+
+    //Calculate the actual income after the standard deduction 
+    //Using the calculated income to check what would be the tax_rate
+    calc_income = gross_income - standard_deduction;
+
+    //Using multiple if, else and else if statement for each tax bracket and filing status
+    //'s' || 'S' = single filing
+    if (filing_status == 'S' || filing_status == 's') {
         if (calc_income <= 11600) {
             calc_income = (calc_income * 0.10);
-            tax_rate = '10%';  }
-        else if  (calc_income >= 11601 && calc_income <= 47150 ) {
+            tax_rate = "10%";
+        } else if  (calc_income >= 11601 && calc_income <= 47150 ) {
             calc_income = (calc_income * 0.12); 
-            tax_rate = '12%'; }
-        else if (calc_income >= 47151 && calc_income <= 100525) {
+            tax_rate = "12%";
+        } else if (calc_income >= 47151 && calc_income <= 100525) {
             calc_income = (calc_income * 0.22);
-            tax_rate = '22%'; }
-        else if (calc_income >= 100526 && calc_income <= 191950) {
+            tax_rate = "22%";
+        } else if (calc_income >= 100526 && calc_income <= 191950) {
             calc_income = (calc_income * 0.24); 
-            tax_rate = '24%'; }
-        else if (calc_income >= 191951 && calc_income <= 243725) {
+            tax_rate = "24%";
+        } else if (calc_income >= 191951 && calc_income <= 243725) {
             calc_income = (calc_income * 0.32); 
-            tax_rate = '32%'; }
-        else if (calc_income >= 243726 && calc_income <= 609350) {
+            tax_rate = "32%";
+        } else if (calc_income >= 243726 && calc_income <= 609350) {
             calc_income = (calc_income * 0.35); 
-            tax_rate = '35%'; }
-        else if (calc_income >= 609351) {
+            tax_rate = "35%";
+        } else if (calc_income >= 609351) {
             calc_income = (calc_income * 0.37); 
-            tax_rate = '37%'; }
-        
- 
+            tax_rate = "37%";
         }
-    
-    if (filing_status == 'H' || filing_status == 'h') { //calc tax for head of household
+    }
+
+    //"H" || "h" = Head of household
+    if (filing_status == 'H' || filing_status == 'h') {
         if (calc_income <= 16550) {
             calc_income = (calc_income * 0.10);  
-            tax_rate = '10%';}
-        else if  (calc_income >= 16551 && calc_income <= 63100 ) {
+            tax_rate = "10%";
+        } else if  (calc_income >= 16551 && calc_income <= 63100 ) {
             calc_income = (calc_income * 0.12); 
-            tax_rate = '12%'; }
-        else if (calc_income >= 63101 && calc_income <= 100500) {
+            tax_rate = "12%";
+        } else if (calc_income >= 63101 && calc_income <= 100500) {
             calc_income = (calc_income * 0.22); 
-            tax_rate = '22%'; }
-        else if (calc_income >= 100501 && calc_income <= 191950) {
+            tax_rate = "22%";
+        } else if (calc_income >= 100501 && calc_income <= 191950) {
             calc_income = (calc_income * 0.24); 
-            tax_rate = '24%'; }
-        else if (calc_income >= 191951 && calc_income <= 243700) {
+            tax_rate = "24%";
+        } else if (calc_income >= 191951 && calc_income <= 243700) {
             calc_income = (calc_income * 0.32); 
-            tax_rate = '32%'; }
-        else if (calc_income >= 243701 && calc_income <= 609350) {
+            tax_rate = "32%";
+        } else if (calc_income >= 243701 && calc_income <= 609350) {
             calc_income = (calc_income * 0.35); 
-            tax_rate = '35%'; }
-        else if (calc_income >= 609351){
+            tax_rate = "35%";
+        } else if (calc_income >= 609351){
             calc_income = (calc_income * 0.37); 
-            tax_rate = '37%'; }
+            tax_rate = "37%";
         }
+    }
 
-    if (filing_status == 'J' || filing_status == 'j') { //calc tax for married filing jointly 
+    //"J" || "j" for married filing jointly
+    if (filing_status == 'J' || filing_status == 'j') {
         if (calc_income <= 23200) {
             calc_income = (calc_income * 0.10);  
-            tax_rate = '10%'; }
-        else if  (calc_income >= 23201 && calc_income <= 94300 ) {
+            tax_rate = "10%";
+        } else if  (calc_income >= 23201 && calc_income <= 94300 ) {
             calc_income = (calc_income * 0.12); 
-            tax_rate = '12%'; }
-        else if (calc_income >= 94301 && calc_income <= 201050) {
+            tax_rate = "12%";
+        } else if (calc_income >= 94301 && calc_income <= 201050) {
             calc_income = (calc_income * 0.22); 
-            tax_rate = '22%'; }
-        else if (calc_income >= 201051 && calc_income <= 383900) {
+            tax_rate = "22%";
+        } else if (calc_income >= 201051 && calc_income <= 383900) {
             calc_income = (calc_income * 0.24); 
-            tax_rate = '24%'; }
-        else if (calc_income >= 383901 && calc_income <= 487450) {
+            tax_rate = "24%";
+        } else if (calc_income >= 383901 && calc_income <= 487450) {
             calc_income = (calc_income * 0.32); 
-            tax_rate = '32%'; }
-        else if (calc_income >= 487451 && calc_income <= 731200) {
+            tax_rate = "32%";
+        } else if (calc_income >= 487451 && calc_income <= 731200) {
             calc_income = (calc_income * 0.35); 
-            tax_rate = '35%'; }
+            tax_rate = "35%";
+        } else if (calc_income >= 731201) {
+            calc_income = (calc_income * 0.37); 
+            tax_rate = "37%";
         }
+    }
 
-    if (filing_status == 'P' || filing_status == 'p') { //calc married file separately
+    //"p" || "P" for married filing separtely
+    if (filing_status == 'P' || filing_status == 'p') {
         if (calc_income <= 11600) {
             calc_income = (calc_income * 0.10); 
-            tax_rate = '10%'; }
-        else if  (calc_income >= 11601 && calc_income <= 47150 ) {
+            tax_rate = "10%";
+        } else if  (calc_income >= 11601 && calc_income <= 47150 ) {
             calc_income = (calc_income * 0.12); 
-            tax_rate = '12%'; }
-        else if (calc_income >= 47151 && calc_income <= 100525) {
+            tax_rate = "12%";
+        } else if (calc_income >= 47151 && calc_income <= 100525) {
             calc_income = (calc_income * 0.22); 
-            tax_rate = '22%'; }
-        else if (calc_income >= 100526 && calc_income <= 191950) {
+            tax_rate = "22%";
+        } else if (calc_income >= 100526 && calc_income <= 191950) {
             calc_income = (calc_income * 0.24); 
-            tax_rate = '24%'; }
-        else if (calc_income >= 191951 && calc_income <= 243725) {
+            tax_rate = "24%";
+        } else if (calc_income >= 191951 && calc_income <= 243725) {
             calc_income = (calc_income * 0.32); 
-            tax_rate = '32%'; }
-        else if (calc_income >= 243726 && calc_income <= 365600) {
+            tax_rate = "32%";
+        } else if (calc_income >= 243726 && calc_income <= 365600) {
             calc_income = (calc_income * 0.35); 
-            tax_rate = '35%'; }
-        else if (calc_income >= 365601){
+            tax_rate = "35%";
+        } else if (calc_income >= 365601){
             calc_income = (calc_income * 0.37); 
-            tax_rate = '37%'; }
-        
- 
+            tax_rate = "37%";
         }
-    return 0;
-    
-    cout << "  " << endl;
-    cout << "gross income: ";
-    cout << "filing status: " << filing_status;
-    cout << "Standard deduction: " << ;
-    cout << "taxable income: " << ;
-    cout << "tax amount : " << ;
-}
+    }
 
+    //Calculating for the Taxable income 
+    //Did not call the calc_income since it is being use to calculate the tax amount not the taxable income
+    double taxable_income = gross_income - standard_deduction;
+
+    cout << "\n";
+    cout << "gross income: " << gross_income << endl;
+    cout << "filing status: " << filing_status << endl;
+    cout << "Standard deduction: " << standard_deduction << endl;
+    cout << "taxable income: " << taxable_income << endl;
+    cout << "tax amount : " << calc_income << " (rate " << tax_rate << ")" << endl;
+
+    return 0;
+}
